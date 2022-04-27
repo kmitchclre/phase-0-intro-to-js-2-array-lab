1d520803dc5ce7068db7ee5d769ab5f6628a3c99
Kenny Clare <kmitchellclare@gmail.com>
2022-04-27 08:39:16 -0700

    commit tree 56184ee1e49995e41c2e489c06874540256a5b69

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Meowzy) {
+    return cats.push(Meowzy)
+}
+
+function destructivelyPrependCat(Louie) {
+    return cats.unshift(Louie);
+}
+
+function destructivelyRemoveLastCat(Meowzy) {
+    return cats.pop(Meowzy);
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+    return cats.shift(Milo);
+}
+
+function appendCat(Ozzy) {
+    return [...cats, Ozzy]
+}
+
+function prependCat(Jack) {
+    return [Jack, ...cats]
+}
+
+
+function removeLastCat(Meowzy) {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(Ozzy) {
+    return cats.slice(1);
+}
